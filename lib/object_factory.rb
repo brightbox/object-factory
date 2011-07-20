60d1f892745f3897847c7586335c92e4c25034a1
Hemant Kumar <gethemant@gmail.com>
2011-07-20 14:01:09 +0530

    add support for after_create callback

--- lib/object_factory.rb
+++ lib/object_factory.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'rujitsu'
 
@@ -6,17 +7,17 @@ class Object
   def self.factory
     THE_OBJECT_FACTORY_INSTANCE
   end
-  
+
   # Factory allows test suites to build new instances of objects, specifying some simple constraints on certain fields
   # If a new instance is created via the factory then that instance can have specialist values automatically applied to given fields, meaning that it should be possible for test cases to build valid objects without having to specify a full valid field-set
-  # The factory should not be created directly, but instead accessed through the Object#factory method.  
-  # Expected usage: 
+  # The factory should not be created directly, but instead accessed through the Object#factory method.
+  # Expected usage:
   #   Object.factory.when_creating_a Person, :auto_generate => [:email, :telephone], :auto_confirm => :password
   #   instance = a Person
-  # instance will have a unique value for :email and :telephone and will ensure that :password and :password_confirmation have the same value.  
+  # instance will have a unique value for :email and :telephone and will ensure that :password and :password_confirmation have the same value.
   class Factory
     attr_accessor :generator
-    
+
     def initialize
       reset
     end
@@ -25,9 +26,10 @@ class Object
     def reset
       @confirmed_fields = {}
       @generators = {}
+      @factory_callbacks = {}
       @classes_for_clean_up = []
     end
-    
+
     # clean up all instances - all classes that are registered for clean up have all instances deleted
     # this is useful if you cannot use transactions to tidy up after each test
     def clean_up
@@ -38,30 +40,38 @@ class Object
         end
       end
     end
-  
+
     # Create a new instance of the given class with the given parameters and apply the auto-generated fields, according to the configured rules
     def create_a klass, parameters = {}
       instance = klass.new parameters
-      
+
       generate_confirmations_for instance, parameters
       generate_values_for instance, parameters
-      
+
       yield(instance) if block_given?
-      
+
       return instance
     end
-    
+
     # Create a new instance of the given class with the given parameters, auto-generate the field values and then call save!
     def create_and_save_a klass, parameters = {}
       block = block_given? ? Proc.new : nil
       instance = create_a klass, parameters, &block
       raise CannotSaveError, instance.errors.inspect unless instance.save
+      call_after_create instance
       return instance
     end
-    
-    # Set up the required auto-generated fields for the given class.  
+
+    def call_after_create instance
+      defined_callbacks = @factory_callbacks[symbol_for(instance)]
+      if(!defined_callbacks.blank? && (callback = defined_callbacks[:after_create]))
+        callback.call(instance)
+      end
+    end
+
+    # Set up the required auto-generated fields for the given class.
     #   Object.factory.when_creating_a MyClass, :auto_generate => [:field1, :field2], :auto_confirm => :password, :generate_email_address => :email_address, :set => { :field3 => 'value3', :field4 => 'value4' }, :generator => { :field5 => lambda {Date.today}, :field6 => lambda {Time.now} }
-    # Options are: 
+    # Options are:
     # * :auto_generate specifies a field name or array of field names that are to have unique string values assigned to them
     # * :auto_confirm specifies a field name or array of field names that are to be set to a unique value; with the same value being assigned to field_name_confirmation
     # * :generate_email_address specifies a field name or array of field names that are set to be randomised email addresses
@@ -70,14 +80,15 @@ class Object
     # * :clean_up specifies whether the class should be registered for clean up (the default is true)
     def when_creating_a klass, options = {}
       need_to_generate_values_for klass, options[:auto_generate] unless options[:auto_generate].nil?
-      need_to_confirm_values_for klass, options[:auto_confirm] unless options[:auto_confirm].nil? 
+      need_to_confirm_values_for klass, options[:auto_confirm] unless options[:auto_confirm].nil?
       need_to_generate_email_addresses_for klass, options[:generate_email_address] unless options[:generate_email_address].nil?
       need_to_generate_ip_addresses_for klass, options[:generate_ip_address] unless options[:generate_ip_address].nil?
-      need_to_set_values_for klass, options[:set] unless options[:set].nil? 
+      need_to_set_values_for klass, options[:set] unless options[:set].nil?
       need_to_set_generators_for klass, options[:generate] unless options[:generate].nil?
+      add_after_create klass, options[:after_create] if options[:after_create]
       register_for_clean_up klass unless options[:clean_up] == false
     end
-    
+
     alias :when_creating_an :when_creating_a
     alias :create_and_save_an :create_and_save_a
 
@@ -85,66 +96,66 @@ class Object
     def generator
       @generator ||= ValueGenerator.new
     end
-    
+
     # Generate a unique Integer
     def next_number
       generator.unique_integer
     end
-    
+
     # A simple class that generates unique values
     class ValueGenerator
       def initialize
         @counter = 0
       end
-      
+
       def unique_integer
         @counter += 1
       end
-      
+
       def value_for klass, field
         "#{klass.name.to_s}-#{field.to_s}-#{unique_integer}"
       end
     end
-    
+
     # Error raised when create_and_save_a cannot save the object
     class CannotSaveError < RuntimeError; end
-    
+
     # print the rules for a given class
     def print_configuration_for klass
       fields_and_generators = @generators[symbol_for(klass)]
-      unless fields_and_generators.nil? 
-        fields_and_generators.each do | field_name, generator | 
+      unless fields_and_generators.nil?
+        fields_and_generators.each do | field_name, generator |
           puts "#{field_name} uses a lambda"
         end
       end
     end
-    
+
   private
-      
+
     def symbol_for object
       klass = object.is_a?(Class) ? object : object.class
       return klass.name.to_sym
     end
-    
+
     def need_to_generate_values_for klass, fields
       fields = [fields] unless fields.respond_to?(:each)
-      fields.each do | field | 
+      fields.each do | field |
         add_generator_for klass, field, lambda { generator.value_for(klass, field) }
       end
     end
-    
+
     def need_to_confirm_values_for klass, fields
       fields = [fields] unless fields.respond_to?(:each)
       @confirmed_fields[symbol_for(klass)] = fields
     end
-    
+
     def need_to_generate_email_addresses_for klass, fields
       fields = [fields] unless fields.respond_to?(:each)
       fields.each do | field |
         add_generator_for klass, field, lambda { 6.random_letters + '@' +  10.random_letters + '.com' }
       end
     end
-    
+
     def need_to_generate_ip_addresses_for klass, fields
       fields = [fields] unless fields.respond_to?(:each)
       fields.each do | field |
@@ -155,9 +166,9 @@ class Object
         }
       end
     end
-    
+
     def need_to_set_values_for klass, fields_and_values
-      fields_and_values.each do | field, value | 
+      fields_and_values.each do | field, value |
         add_generator_for klass, field, lambda { value }
       end
     end
@@ -166,16 +177,21 @@ class Object
       @generators[symbol_for(klass)] ||= {}
       @generators[symbol_for(klass)][field] = generator
     end
-    
+
+    def add_after_create klass, after_create_block
+      @factory_callbacks[symbol_for(klass)] ||= {}
+      @factory_callbacks[symbol_for(klass)][:after_create] = after_create_block
+    end
+
     def need_to_set_generators_for klass, fields_and_generators
       fields_and_generators.each do | field, generator |
         add_generator_for klass, field, generator
       end
     end
-    
+
     def generate_confirmations_for instance, parameters
       field_names = @confirmed_fields[symbol_for(instance)]
-      return if field_names.nil? 
+      return if field_names.nil?
       field_names.each do | field_name |
         confirmation_field_name = "#{field_name}_confirmation"
         value = generator.value_for(instance.class, field_name)
@@ -183,27 +199,27 @@ class Object
         instance.send("#{confirmation_field_name.to_s}=".to_sym, value) unless parameters.has_key?(confirmation_field_name.to_sym)
       end
     end
-    
+
     def generate_values_for instance, parameters
       fields_and_generators = @generators[symbol_for(instance)]
       return if fields_and_generators.nil?
-      fields_and_generators.each do | field_name, proc | 
+      fields_and_generators.each do | field_name, proc |
         unless parameters.has_key?(field_name.to_sym)
           value = proc.call
           instance.send("#{field_name.to_sym}=".to_sym, value)
         end
       end
     end
-    
+
     def register_for_clean_up klass
       @classes_for_clean_up << klass
     end
   end
-  
+
 end
 
 # Short-cut method for Object::Factory#create_a
-# Also aliased as an, for class names that start with a vowel.  
+# Also aliased as an, for class names that start with a vowel.
 #   instance = a Thingy
 #   another_instance = an OtherThingy
 def a klass, parameters = {}
