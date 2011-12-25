20ec66e08ea44dd043a1577474efaa2e71c255e0
Caius Durling <caius@brightbox.co.uk>
2011-12-25 11:57:56 +0000

    Make TemplateCollection#classes_for_cleaning work with 1.8.7

--- lib/object/factory/template_collection.rb
+++ lib/object/factory/template_collection.rb
@@ -9,14 +9,18 @@ class Object
       # If there isn't one defined, it creates one on the fly for you, but doesn't
       # store it in the template collection
       #
-      # @return [Object::Factory::Template]
+      # @return Object::Factory::Template
       def template_for klass
         find {|t| t.klass == klass } || Template.new(:klass => klass)
       end
 
+      # Returns all the classes to delete records for when Factory#clean_up is invoked
+      # 
+      # @return Array
       def classes_for_cleaning
-        to_a.select {|t| t.clean_up }.map(&:klass)
+        __getobj__.to_a.select {|t| t.clean_up }.map(&:klass)
       end
+
     end
   end
 end
