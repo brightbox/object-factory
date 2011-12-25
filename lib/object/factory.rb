5c3aaafbb7a2b912430375b8a40f68676ca2f3ed
Caius Durling <caius@brightbox.co.uk>
2011-12-25 10:43:16 +0000

    Putting Objects in your Factory
    Start of a complete rewrite of the factory, leveraging things from ruby core & stdlib for epic wins and proper objects for everything else.

---
+++ lib/object/factory.rb
@@ -0,0 +1,32 @@
+class Object
+  class Factory
+    include Singleton
+    extend Forwardable
+
+    autoload :ValueGenerator,     "object/factory/value_generator"
+
+    def_delegators :generator, :value_for, :unique_integer, :next_number, :a_number
+
+    CannotSaveError = Class.new(RuntimeError)
+
+    attr_writer :generator
+
+    def generator
+      @generator ||= ValueGenerator.new
+    end
+
+    def reset
+      @generator = nil
+    end
+
+    def when_creating_a klass, params={}
+    end
+    alias when_creating_an when_creating_a
+
+    def create_a klass, params={}, &block
+    end
+
+    def create_and_save_a *args, &block
+    end
+  end
+end
